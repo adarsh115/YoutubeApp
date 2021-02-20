ada206ec3b52a2f330b2b7ca02cb12f79e2d6a06
Adarsh <44157141+adarsh115@users.noreply.github.com>
2021-02-20 12:17:41 +0530

    Searchbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import { Component } from "react";
+import SearchBar from "./components/SearchBar";
+import { Container } from "react-bootstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <Container
+        style={{ backgroundColor: "lightsalmon", padding: "10px" }}
+        fluid="lg"
+      >
+        <SearchBar />
+      </Container>
+    );
+  }
 }
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Button, Form } from "react-bootstrap";
+
+class SearchBar extends Component {
+  state = {
+    term: ""
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      term: e.target.value
+    });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // todo make sure we call callback from parent
+  };
+
+  render() {
+    return (
+      <div className="search-bar">
+        <form onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label
+              style={{
+                fontFamily: "sans-serif"
+              }}
+            >
+              Video Search
+            </label>
+            <br />
+            <input
+              value={this.state.term}
+              style={{
+                border: "0.1px solid grey",
+                marginTop: "5px",
+                padding: "3px"
+              }}
+              onChange={this.handleInputChange}
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
